d12b6a9d428a9e2e10c986b346a35c32011d12e7
maxmaxim6 <57861712+maxmaxim6@users.noreply.github.com>
2020-07-25 15:33:38 +0300

    Update Demo.cpp

--- Demo.cpp
+++ Demo.cpp
@@ -10,19 +10,19 @@
 #include <stdexcept>
 using namespace std;
 
-// int main() 
-// {
-// 	string text = "TOND vorri be haffy";
-// 	cout << phonetic::find(text, "dont") << endl;   // should print "Dond"
-// 	cout << phonetic::find(text, "worry") << endl;   // should print "vorri"
-// 	cout << phonetic::find(text, "Be") << endl;   // should print "be"
-// 	cout << phonetic::find(text, "Pe") << endl;   // should print "be"
-// 	cout << phonetic::find(text, "happy") << endl;   // should print "haffy"
-// 	try {
-// 		cout << phonetic::find(text, "hap") << endl;   // should throw an exception - "happ" is not a full word in the sentence
-// 	} 
-// 	catch (exception& ex) 
-// 	{
-// 	 	cout << "caught exception: " << ex.what() << endl;  // should print "Did not find the word 'happ' in the text"
-// 	}
-// }
+ int main() 
+ {
+ 	string text = "TOND vorri be haffy";
+ 	cout << phonetic::find(text, "dont") << endl;   // should print "Dond"
+ 	cout << phonetic::find(text, "worry") << endl;   // should print "vorri"
+ 	cout << phonetic::find(text, "Be") << endl;   // should print "be"
+ 	cout << phonetic::find(text, "Pe") << endl;   // should print "be"
+ 	cout << phonetic::find(text, "happy") << endl;   // should print "haffy"
+ 	try {
+ 		cout << phonetic::find(text, "hap") << endl;   // should throw an exception - "happ" is not a full word in the sentence
+ 	} 
+ 	catch (exception& ex) 
+ 	{
+ 	 	cout << "caught exception: " << ex.what() << endl;  // should print "Did not find the word 'happ' in the text"
+ 	}
+ }
